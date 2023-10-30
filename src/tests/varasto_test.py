5d11ff4dabd4f6be3bdeb80f6ef4d80e20bc7abd
Juho <juho_tuomas@msn.com>
2023-10-30 13:41:19 +0200

    added tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,6 +10,18 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_uusi_varasto_ei_positiivinen_tilavuus(self):
+        self.varasto2 = Varasto(-1)
+        self.assertAlmostEqual(self.varasto2.tilavuus, 0)
+
+    def test_uusi_varasto_ei_positiivinen_tilavuus_saldo(self):
+        self.varasto2 = Varasto(0, -1)
+        self.assertAlmostEqual(self.varasto2.saldo, 0)
+
+    def test_uusi_varasto_tilavuus_saldo_samat(self):
+        self.varasto2 = Varasto(1, 2)
+        self.assertAlmostEqual(self.varasto2.saldo, 1)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
@@ -17,6 +29,16 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisays_lisaa_saldoa_miinus(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_lisays_lisaa_saldoa_liikaa(self):
+        self.varasto.lisaa_varastoon(11)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +60,15 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_otetaan_miinus(self):
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_otetaan_liikaa(self):
+        self.varasto.ota_varastosta(1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_str_funktio_toimii(self):
+        print(self.varasto)
+    
